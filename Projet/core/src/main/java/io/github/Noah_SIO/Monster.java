c869eeb2b20e387fe505d79d60613b246c8dcf13
Noah_FGC <laplaze.noah@gmail.com>
2025-03-24 11:49:54 +0100

    ajout class monster et fonction

--- Projet/core/src/main/java/io/github/Noah_SIO/Monster.java
+++ Projet/core/src/main/java/io/github/Noah_SIO/Monster.java
@@ -8,5 +8,41 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Monster {
-    
+    private float x;
+    private float y;
+    private int size;
+    private int type; ///1,2,3
+
+
+    public Monster(float x, float y, int size, int type){
+        this.x=x;
+        this.y=y;
+        this.size=size;
+        this.type=type;
+    }
+
+
+
+    public void move(float x, float y){
+        this.x += x;
+        this.y += y;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public float getSize(){
+        return size;
+    }
+
+    public void draw(ShapeRenderer shape){
+        shape.circle(x,y,size);
+    }
+
+
 }
