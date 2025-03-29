5daa61258b86e81d2a059501b6dae62380b0cb2e
Noah_FGC <laplaze.noah@gmail.com>
2025-03-29 15:33:20 +0100

    modif launcher

--- Projet/lwjgl3/src/main/java/io/github/Noah_SIO/lwjgl3/Lwjgl3Launcher.java
+++ Projet/lwjgl3/src/main/java/io/github/Noah_SIO/lwjgl3/Lwjgl3Launcher.java
@@ -20,20 +20,17 @@ public class Lwjgl3Launcher {
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
         configuration.setTitle("DragonGaiden");
+        
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
+        configuration.useVsync(false);
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
-        // Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        // int screenWidth =(int) (screenSize.width/2.1);
-        // int screenHeight = (int) (screenSize.height/1.7);
-        // configuration.setWindowedMode(screenWidth,screenHeight);
-        configuration.setWindowedMode(450,450);
+        configuration.setWindowedMode(800,600);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
